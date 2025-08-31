4f3b6fdcfe0ed4bbdfe504178d8cd5d5908f537d
Shanjai Raj <53368803+Rajsuthan@users.noreply.github.com>
2025-08-31 17:14:58 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          backendUrl: process.env.VITE_APP_BACKEND_URL,
+          projectId: process.env.VITE_APP_PROJECT_ID,
+          hostApi: "localhost:8084",
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
