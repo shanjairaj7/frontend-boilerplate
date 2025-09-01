141e36410f33c40c5fe853b0d721b219321acb5f
Shanjai Raj <53570970+shanjairaj7@users.noreply.github.com>
2025-09-01 08:50:47 +0530

    Update vite.config.ts with env variable
    Previously it didnt import the env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,31 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          backendUrl: process.env.VITE_APP_BACKEND_URL,
-          projectId: process.env.VITE_APP_PROJECT_ID,
-          hostApi: "localhost:8084",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            backendUrl: env.VITE_APP_BACKEND_URL || '',
+            projectId: env.VITE_APP_PROJECT_ID || '',
+            hostApi: "localhost:8084",
+          },
         },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+  };
 });
